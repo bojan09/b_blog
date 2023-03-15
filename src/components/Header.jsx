bdb16b31dff25bead216a0a12810ca7a7b1cb019
Bojan <bojan-stanimirovski097@hotmail.com>
2023-03-15 22:36:37 +0100

    initial setup

---
+++ src/components/Header.jsx
@@ -0,0 +1,41 @@
+// context
+import React, { useContext } from "react";
+
+// next link
+import Link from "next/link";
+
+// demo categories
+const categories = [
+  { name: "Bitola", slug: "Bitola is a nice city" },
+  { name: "Ohrid", slug: "Ohrid has a natural lake" },
+];
+
+const Header = () => {
+  return (
+    <div className="container mx-auto px-10 mb-8">
+      <div className="border-b w-full inline-block border-blue-400 py-8">
+        {/* Logo */}
+        <div className="md:float-left block">
+          <Link href="/">
+            <span className="cursor-pointer font-bold text-4xl text-white">
+              Graph CMS
+            </span>
+          </Link>
+        </div>
+
+        {/* Categories */}
+        <div className="hidden md:float-left md:contents">
+          {categories.map((category) => (
+            <Link key={category.slug} href={`/category/${category.slug}`}>
+              <span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">
+                {category.name}
+              </span>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,16 @@
-import '@/styles/globals.css'
+// styles
+import "@/styles/globals.scss";
+
+// state & effect
+import React, { useState, useEffect } from "react";
+
+// layout component
+import { Layout } from "../components";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
